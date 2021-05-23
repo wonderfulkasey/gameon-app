70abb50bfe187796944f6a9b97cb2402ab1dedc8
wonderfulkasey <kaseyarnold15@gmail.com>
2021-05-23 15:52:18 -0400

    added blank picks controller

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -1,2 +1,34 @@
 class PicksController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end 
+
+    def new 
+    end 
+
+    def create 
+    end 
+
+    def edit
+    end 
+
+    def update
+    end 
+
+    def destroy
+    end 
+
+    private 
+
+    def pick_params
+        params.require(:pick).permit(
+            :team_1,
+            :team_2,
+            :sport,
+            :time,
+        )
+
 end
