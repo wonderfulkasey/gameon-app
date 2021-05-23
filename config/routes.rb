3d4f87ed1e7ee351369ab1a18c8d643fd242f23f
wonderfulkasey <kaseyarnold15@gmail.com>
2021-05-23 15:41:49 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #home pages
+  root "picks#index"
+  
+  #for all pick routes
+  resources :picks
+
 end
