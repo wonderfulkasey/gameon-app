fc63b3eee0155e054d666b93f48ac31691c50d11
wonderfulkasey <kaseyarnold15@gmail.com>
2021-05-24 10:12:01 -0400

    added rest of code to picks controller

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -1,24 +1,47 @@
 class PicksController < ApplicationController
 
     def index
+        @picks = Pick.all
     end
 
     def show
+        pick = Pick.find(params[:id])
     end 
 
     def new 
+        @pick = Pick.new
     end 
 
     def create 
+        if @pick.save
+            flash[:notice] = "Pick was created."
+            redirect_to pick_path(@pick)
+        else  
+            flash[:alert] = "Pick not created. Try again."
+            render :new
+        end 
     end 
 
     def edit
+        @pick = Pick.find(params[:id])
     end 
 
     def update
+        @pick = Pick.find(params[:id])
+
+        if @pick.update(pick_params)
+            redirect_to pick_path(@pick)
+        else  
+            flash[:errors] = @pick.errors.full_messages
+            render :edit
+        end 
     end 
 
     def destroy
+        @pick = Pick.find(params[:id])
+        @pick.destroy 
+
+        redirect_to picks_path
     end 
 
     private 
@@ -30,5 +53,6 @@ class PicksController < ApplicationController
             :sport,
             :time
         )
+    end 
 
 end
